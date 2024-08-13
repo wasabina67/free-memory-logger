1a644582331ce5f1bae3f9b00b62a647a19acad4
wasabina67 <wasabina67@gmail.com>
2024-08-13 11:05:01 +0900

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def main():
-    pass
+    df = pd.read_csv("output.csv")
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(df["timestamp"], df["free_memory"])
+
+    plt.title("free-memory-logger")
+    plt.xlabel("Time")
+    plt.ylabel("Free Memory (MB)")
+    plt.grid(True)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+
+    plt.savefig("output.png")
 
 
 if __name__ == "__main__":
