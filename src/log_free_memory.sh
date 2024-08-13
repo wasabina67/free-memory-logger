27ee7a36fbf5931ab5260cdd1b8f792b2be77a90
wasabina67 <wasabina67@gmail.com>
2024-08-13 10:44:00 +0900

    Update log_free_memory.sh

--- src/log_free_memory.sh
+++ src/log_free_memory.sh
@@ -2,3 +2,16 @@
 
 OUTPUT_CSV='output.csv'
 INTERVAL=${INTERVAL:-1}
+
+function handle_sigint() {
+    echo -e "\nSIGINT received, exiting..."
+    exit 0
+}
+
+trap handle_sigint SIGINT
+
+function log_free_memory() {
+    local timestamp=$(date --iso-8601=seconds)
+    local free_memory=$(free --mega | awk '/^Mem:/{print $4}')
+    echo "${timestamp},${free_memory}"
+}
